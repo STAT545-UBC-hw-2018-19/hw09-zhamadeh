b410e078ce05e3255cf368d1c21189abbe6c4848
Zeid Hamadeh <zeidh@Zeids-MacBook-Pro.local>
2018-11-27 23:08:34 -0800

    new

--- letter_count.R
+++ letter_count.R
@@ -1,13 +1,13 @@
 library(stringr)
+
+#take in words file
 words <- readLines("words.txt")
 
+#create table using letters as levels and tallying the letter composition of each word using strsplit
 letter_count <- table(factor(unlist(strsplit(words, ""), use.names=FALSE), levels=letters))
 
+#converting the table to a TSV file
 write.table(letter_count, "letter_count.tsv",
 			sep = "\t", row.names = FALSE, quote = FALSE)
 
-t <- read.delim("letter_count.tsv")
-
-ggplot(t) + geom_bar(stat= "identity", aes(x = Var1, y= Freq)) +
-	labs(x="Letter", y="Frequency")
 
